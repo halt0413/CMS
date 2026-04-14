01d646f80f25a4dcb4e6ae414b06d9200a414dc0
hal <halt0413@gmail.com>
2026-04-14 16:28:59 +0900

    コンテンツ編集機能の実装: 更新ミューテーションを追加し、フォームの送信処理を実装

--- apps/cms/src/routes/contents/api/index.ts
+++ apps/cms/src/routes/contents/api/index.ts
@@ -1,3 +1,4 @@
 export { getContent } from "./getContent";
 export { listContents } from "./listContents";
+export { updateContent } from "./updateContent";
 export type { ContentItem } from "./contentItem";
